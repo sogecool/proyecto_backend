1686a986600f2ae0c8c2548f48ceaded53c6638b
Aaron Hernandez <ksamsarojam333@gmail.com>
2024-07-02 10:32:21 -0700

    Agregamos el Index a nuestro proyecto de Backend I

---
+++ index.php
@@ -0,0 +1,60 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <title>BACKEND I -> PROYECTO DE PRÁCTICAS</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <h1>BACKEND I -> PROYECTO DE PRÁCTICAS</h1>
+
+    <div class="unidad">
+        <h2>Unidad 1</h2>
+        <ul>
+            <!-- TODAS LAS PRÁCTICAS Y TAREAS DE LA UNIDAD 1 -->
+            <li><a href="unidad_1/U1.A1_LenguajeImperativo.php">ACTIVIDAD 1 -> LENGUAJES IMPERATIVOS</a></li>
+            <li><a href="unidad_1/U1.A2_TemasDatos.php">ACTIVIDAD 2 -> TEMAS DATOS NÚMERICOS Y STRINGS</a></li>
+            <li><a href="unidad_1/U1.A3_Expresiones.php">ACTIVIDAD 3 -> EXPRESIONES Y OPERACIONES</a></li>
+            <li><a href="unidad_1/U1.A4_Strings_Textos.php">ACTIVIDAD 4 -> STRINGS Y TEXTOS</a></li>
+            <li><a href="unidad_1/U1.A5.1_AlmacenarVariable.php">ACTIVIDAD 5 -> VARIABLES Y OPERADORES: ALMACENAR VARIABLES</a></li>
+            <li><a href="unidad_1/U1.A5.2_AreaTriangulo.php">ACTIVIDAD 5 -> VARIABLES Y OPERADORES: ÁREA DE UN TRIÁNGULO</a></li>
+            <li><a href="unidad_1/U1.A5.3_EdadVariable.php">ACTIVIDAD 5 -> VARIABLES Y OPERADORES: EDAD VARIABLE</a></li>
+            <li><a href="unidad_1/U1.A6_Operaciones_Strings.php">ACTIVIDAD 6 -> OPERACIONES Y STRINGS</a></li>
+            <li><a href="unidad_1/U1.A7_Funciones.php">ACTIVIDAD 7 -> FUNCIONES</a></li>
+            <!-- Añade más enlaces según sea necesario -->
+        </ul>
+    </div>
+
+    <div class="unidad">
+        <h2>Unidad 2</h2>
+        <ul>
+            <!-- TODAS LAS PRÁCTICAS Y TAREAS DE LA UNIDAD 2 -->
+            <li><a href="unidad_2/U2.A1.1_PrácticaIF.php">ACTIVIDAD 1 -> USO DE IF, FOR & VECTORES: PRÁCTICA IF</a></li>
+            <li><a href="unidad_2/U2.A1.2_PrácticaFOR.php">ACTIVIDAD 1 -> USO DE IF, FOR & VECTORES: PRÁCTICA FOR</a></li>
+            <li> <a href="unidad_2/U2.A1.3_PrácticaVectores.php">ACTIVIDAD 1 -> USO DE IF, FOR & VECTORES: PRÁCTICA DE VECTORES</a></li>
+            <li> <a href="unidad_2/U2.A2_POST_GET_SERVER.php">ACTIVIDAD 2 -> MÉTODO POST, GET & SERVER</li>
+            <li> <a href="unidad_2/U2.A3.1_Práctica_1_al_10.php">ACTIVIDAD 3 -> ESTRCUTURAS DE CONTROL: PRINT DEL 1 AL 10</a></li>
+            <li> <a href="unidad_2/U2.A3.2_Práctica_PAR_o_IMPAR.php">ACTIVIDAD 3 -> ESTRCUTURAS DE CONTROL: PAR O IMPAR</a></li>
+            <li> <a href="unidad_2/U2.A3.3_PrácticaWHILE.php">ACTIVIDAD 3 -> ESTRCUTURAS DE CONTROL: PRÁCTICA WHILE</a></li>
+            <li> <a href="unidad_2/U2.A4.1_Estudiantes.php">ACTIVIDAD 4 -> ARRAYS: ARRAY DE ESTUDIANTES</a></li>
+            <li> <a href="unidad_2/U2.A4.2_PromedioCalif.php">ACTIVIDAD 4 -> ARRAYS: ARRAY DE CALIFICACIONES</a></li>
+            <li> <a href="unidad_2/U2.A4.3_ArrayNúmeros.php">ACTIVIDAD 4 -> ARRAYS: ARRAY DE NÚMEROS</a></li>
+            <li> <a href="unidad_2/U2.A5_CuadroComparativo.php">ACTIVIDAD 5 -> CUADRO COMPARATIVO</a></li>
+            <li> <a href="unidad_2/U2.A6_HASHES.php">ACTIVIDAD 6 -> INFOGRAFÍA HASHES</a></li>
+            <!-- Añade más enlaces según sea necesario -->
+        </ul>
+    </div>
+
+    <div class="unidad">
+        <h2>Unidad 3</h2>
+        <ul>
+            <!-- TODAS LAS PRÁCTICAS Y TAREAS DE LA UNIDAD 3 -->
+            <li><a href="unidad_3/U3.A1_Formulario.php">ACTIVIDAD 1 -> FORMULARIO</a></li>
+            <li><a href="unidad_3/U3.A2_FormularioRegistro.php">ACTIVIDAD 2 -> FORMULARIO - REGISTRO</a></li>
+            <li><a href="unidad_3/U3.A3_POO.php">ACTIVIDAD 3 -> INVESTIGACIÓN POO</a></li>
+            <!-- Añade más enlaces según sea necesario -->
+        </ul>
+    </div>
+
+</body>
+</html>
